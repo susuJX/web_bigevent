a646b62866f76e800dc02de900d533a340e076a7
susuJX <susu9407@126.com>
2022-02-27 21:10:29 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function() {
+    getUserinfo()
+
+    var layer = layui.layer
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户的基本信息
+function getUserinfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        },
+        // 不论成功还是失败，最终都会调用 complete 回调函数
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('.welcome').html('欢迎&nbsp&nbsp' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
